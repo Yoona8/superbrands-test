e8d535482f39bc4cf83896e3d6194132a557f843
Yoona8 <elena243v@gmail.com>
2020-05-25 14:43:05 +0300

    Add разметку и стили выбора специалиста

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ManagersComponent } from './components/managers/managers.component';
+import { ButtonAddComponent } from './components/shared/button-add/button-add.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ManagersComponent,
+        ButtonAddComponent
     ],
     imports: [
         BrowserModule
---
+++ src/app/components/managers/managers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-managers',
+    templateUrl: './managers.component.html',
+    styleUrls: ['./managers.component.css']
+})
+export class ManagersComponent {
+
+}
