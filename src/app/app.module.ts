9a107b905543ea9748bc8b024eb142711a25ac13
Yoona8 <elena243v@gmail.com>
2020-05-25 18:47:56 +0300

    Add компонент списка магазинов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ManagersComponent } from './components/managers/managers.component';
 import { ButtonAddComponent } from './components/shared/button-add/button-add.component';
 import { ButtonCheckComponent } from './components/shared/button-check/button-check.component';
+import { ShopsComponent } from './components/shops/shops.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ManagersComponent,
         ButtonAddComponent,
-        ButtonCheckComponent
+        ButtonCheckComponent,
+        ShopsComponent
     ],
     imports: [
         BrowserModule
---
+++ src/app/components/shops/shops.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-shops',
+    templateUrl: './shops.component.html',
+    styleUrls: ['./shops.component.css']
+})
+export class ShopsComponent {}
